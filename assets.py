e8e36648faec447ccf07aa2939aa8b58b99b1faf
luccampl <luccampl@al.insper.edu.br>
2021-06-07 16:37:26 -0300

    descricao_assets/lucca

--- assets.py
+++ assets.py
@@ -24,17 +24,29 @@ def load_assets():
     assets = {}
     assets[IMG_TEST] = pygame.image.load(path.join(IMG,'img_test.png')).convert()
     assets[BG_TEST] = pygame.image.load(path.join(IMG,'bg_test.png')).convert()
+    # Adiciona a imagem do avião do jogador:
     assets[IMG_PLAYER_TEST] = pygame.image.load(path.join(IMG,'aviao2.png')).convert_alpha()
+    # Define o tamanho do avião do jogador:
     assets[IMG_PLAYER_TEST] = pygame.transform.scale(assets[IMG_PLAYER_TEST], (194, 110))
+    # Adiciona a imagem do "tiro":
     assets[IMG_BULLET_TEST] = pygame.image.load(path.join(IMG,'bullet.png')).convert_alpha()
+    # Define o tamanho do "tiro":
     assets[IMG_BULLET_TEST] = pygame.transform.scale(assets[IMG_BULLET_TEST], (40, 12))
+    # Adiciona a imagem do avião inimigo:
     assets[IMG_ENEMY_TEST] = pygame.image.load(path.join(IMG,'aviao3.png')).convert_alpha()
+    # Define o tamanho do avião inimigo:
     assets[IMG_ENEMY_TEST] = pygame.transform.scale(assets[IMG_ENEMY_TEST], (118, 42))
+    # Adiciona uma imagem transparente para a "animação" de "piscar" o avião:
     assets['blink'] = pygame.image.load(path.join(IMG,'img_blink.png')).convert_alpha()
+    # Adiciona a imagem da nuvem:
     assets['nuvem'] = pygame.image.load(path.join(IMG,'Nuvem.png')).convert_alpha()
+    # Define o tamanho da nuvem:
     assets['nuvem'] = pygame.transform.scale(assets['nuvem'], (300, 165))
+    # Adiciona o background da loja:
     assets['BGLoja'] = pygame.image.load(path.join(IMG,'Backgroundloja.png')).convert()
+    # Define o tamanho do background da loja:
     assets['BGLoja'] = pygame.transform.scale(assets['BGLoja'], (1280, 720))
+    
     assets['BGTitle'] = pygame.image.load(path.join(IMG,'Titletransparentbg.png')).convert_alpha()
     assets['BGTitle'] = pygame.transform.scale(assets['BGTitle'], (1280, 720))
     assets[FONT] = pygame.font.Font(None,28)
@@ -55,6 +67,7 @@ def load_assets():
         heart_anim.append(img)
     assets["heart_anim"] = heart_anim
 
+    # Adiciona música ao jogo
     pygame.mixer.music.load(path.join(SND, 'Gamemusic.mp3'))
     pygame.mixer.music.set_volume(0.03)
 
